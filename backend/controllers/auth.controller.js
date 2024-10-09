985ab2bf5e1326ddc49e3de9cf534cd389cfb4a4
Liza Kosterieva <prostlizok@gmail.com>
2024-10-10 02:30:23 +0300

    Signup endpoint was added.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,43 @@
+import { User } from '../models/user.model.js'
+import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js'
+
 export const signup = async (req, res) => {
-    res.send("signup route")
+    const { email, name } = req.body;
+    try {
+        if (!email || !name) {
+            throw new Error("All fields are required");
+        }
+
+        const userAlreadyExists = await User.findOne({email});
+        if (userAlreadyExists) {
+            res.status(400).json({success: false, message: "User already exists"})
+        }
+
+        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+
+        const user = new User ({
+            email, 
+            name, 
+            verificationToken,
+            verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000
+        })
+
+        await user.save();
+        
+        // jwt 
+        generateTokenAndSetCookie(res, user._id)
+
+        res.status(201).json({
+            success: true,
+            message: "User created successfully",
+            user: {
+                ...user._doc
+            }
+        })
+
+    } catch (error) {
+        res.status(400).json({success: false, message: error.message})
+    }
 }
 
 export const login = async (req, res) => {
--- backend/index.js
+++ backend/index.js
@@ -8,13 +8,13 @@ import authRoutes from "./routes/auth.route.js"
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(express.json())
 
 app.get("/", (req, res) => {
     res.send("Hello world!")
 });
 
 app.use("/api/auth", authRoutes)
-
 
 app.listen(PORT, () => {
     connectDB();

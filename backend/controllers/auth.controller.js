6a1947f2c438d778f9cb4549c2b4275a8cc80ad6
Liza Kosterieva <prostlizok@gmail.com>
2024-10-10 03:49:20 +0300

    Login endpoint implementation

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -44,8 +44,30 @@ export const signup = async (req, res) => {
 }
 
 export const login = async (req, res) => {
-    res.send("login route")
-}
+	const { email } = req.body;
+	try {
+		const user = await User.findOne({ email });
+		if (!user) {
+			return res.status(400).json({ success: false, message: "Invalid credentials" });
+		}
+
+		generateTokenAndSetCookie(res, user._id);
+
+		user.lastLogin = new Date();
+		await user.save();
+
+		res.status(200).json({
+			success: true,
+			message: "Logged in successfully",
+			user: {
+				...user._doc,
+			},
+		});
+	} catch (error) {
+		console.log("Error in login ", error);
+		res.status(400).json({ success: false, message: error.message });
+	}
+};
 
 export const logout = async (req, res) => {
     res.clearCookie("token");
